83d4feb3db5bf58c0fac082c136ebc8d8847d64f
spencerhum04 <spencerhum04@gmail.com>
2024-11-16 22:35:45 -0500

    feat: component and props for the blog

--- pages/index.vue
+++ pages/index.vue
@@ -50,13 +50,13 @@
 
     const exalts = [
         { potion: "/potion-attack.png", dungeon1: "/dungeon-spectral-penitentiary.png", boss1: "/enemy-soulwarden-murcian.png", dungeon2: "/dungeon-moonlight-village.png", boss2: "/enemy-dancer-miko.png", dungeon3: "/dungeon-the-shatters.png", boss3: "/enemy-twilight-archmage.png" },
-        { potion: "/potion-defense.png", dungeon1: "/dungeon-lost-halls.png", boss1: "/enemy-marble-colossus.png", dungeon2: "/dungeon-the-shatters.png", boss2: "/enemy-the-forgotten-king.png" },
+        { potion: "/potion-defense.png", dungeon1: "/dungeon-lost-halls.png", boss1: "/enemy-marble-colossus.png" },
         { potion: "/potion-speed.png", dungeon1: "/dungeon-cultist-hideout.png", boss1: "/enemy-malus.png" },
         { potion: "/potion-dexterity.png", dungeon1: "/dungeon-the-nest.png", boss1: "/enemy-killer-bee-queen.png", dungeon2: "/dungeon-the-advanced-nest.png", boss2: "/enemy-killer-bee-queen.png" },
         { potion: "/potion-vitality.png", dungeon1: "/dungeon-kogbold-steamworks.png", boss1: "/enemy-factory-control-core.png", dungeon2: "/dungeon-advanced-kogbold-steamworks.png", boss2: "/enemy-factory-control-core.png" },
         { potion: "/potion-wisdom.png", dungeon1: "/dungeon-fungal-cavern.png", boss1: "/enemy-crystal-worm-mother.png", dungeon2: "/dungeon-crystal-cavern.png", boss2: "/enemy-crystal-entity.png" },
         { potion: "/potion-life.png", dungeon1: "/dungeon-oryxs-sanctuary.png", boss1: "/enemy-oryx-3.png", dungeon2: "/dungeon-moonlight-village.png", boss2: "/enemy-sage-genji.png", dungeon3: "/dungeon-the-shatters.png", boss3: "/enemy-the-forgotten-king.png", },
-        { potion: "/potion-mana.png", dungeon1: "/dungeon-the-void.png", boss1: "/enemy-void-entity.png", dungeon2: "/dungeon-moonlight-village.png", boss2: "/enemy-drummer-kaguya.png" },
+        { potion: "/potion-mana.png", dungeon1: "/dungeon-the-void.png", boss1: "/enemy-void-entity.png", dungeon2: "/dungeon-moonlight-village.png", boss2: "/enemy-drummer-kaguya.png", dungeon3: "/dungeon-the-shatters.png", boss3: "/enemy-the-forgotten-king.png" },
     ]
 </script>
 
@@ -78,7 +78,7 @@
             </div>
         </div>
 
-        <div class="flex flex-col items-center bg-light-sand w-full min-h-screen justify-center px-10 py-10">
+        <div class="flex flex-col items-center bg-light-sand w-full min-h-screen justify-center px-10 text-center py-10">
             <div class="flex flex-col items-center border rounded-3xl w-full bg-exalt-gray gap-y-2.5 px-10 py-10">
                 <img src="/pet-oryx-exalted.png" />
                 <div class="text-5xl font-semibold text-white">Stat Exaltations</div>
@@ -106,34 +106,23 @@
         </div>
 
         <div class="flex flex-col items-center gap-y-16 bg-light-sand w-full min-h-screen justify-center py-10">
-            <div class="text-5xl font-semibold">Discover What's New</div>
+            <div class="text-5xl font-semibold text-center">Discover What's New</div>
             <div class="flex flex-wrap gap-6 pt-4">
-                <NuxtLink
+                <Blog
                     v-for="(blog) in blogs"
-                    :href="blog.link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="bg-white p-2.5 rounded-2xl w-[22rem] h-[29rem] flex flex-col"
-                >
-                    <NuxtImg :src="blog.image" class="rounded-lg bg-slate-300 w-[22rem] h-40" />
-                    <div class="flex flex-col px-4 justify-between h-full">
-                        <div class="flex flex-col">
-                            <div class="flex flex-row items-center justify-between text-sm font-semibold text-slate-700 py-4">
-                                <div>Blog</div>
-                                <div>{{ blog.date }}</div>
-                            </div>
-                            <div class="text-xl font-semibold pb-2.5">{{ blog.title }}</div>
-                            <div class="text-sm text-slate-700 pb-4">{{ blog.body }}</div>
-                        </div>
-                        <div class="font-medium underline pb-5">Read Blog</div>
-                    </div>
-                </NuxtLink>
+                    :image="blog.image"
+                    :title="blog.title"
+                    :date="blog.date"
+                    :body="blog.body"
+                    :link="blog.link"
+                />
             </div>
             <NuxtLink
                 :href="'https://remaster.realmofthemadgod.com/?page_id=15'"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="text-blood-red font-medium hover:underline cursor-pointer">View more resources</NuxtLink>
+                class="text-blood-red font-medium hover:underline cursor-pointer">View more resources
+            </NuxtLink>
         </div>
         
     </div>
---
+++ pages/the-shatters/index.vue
@@ -0,0 +1,90 @@
+<script lang="ts" setup>
+
+let bosses = [
+    { image: "/enemy-the-bridge-sentinel.png", link: "/the-shatters/the-bridge-sentinel" },
+    { image: "/enemy-twilight-archmage.png", link: "/the-shatters/twilight-archmage" },
+    { image: "/enemy-the-forgotten-king.png", link: "/the-shatters/the-forgotten-king" },
+];
+
+let loot = [
+    { image: "/enemy-the-bridge-sentinel.png" },
+    { image: "/enemy-twilight-archmage.png.png" },
+    { image: "/enemy-the-forgotten-king" },
+    { image: "/dungeon-the-shatters" },
+]
+
+let bridgeLoot = [
+    { loot: "/item-valor.png" },
+    { loot: "/item-vanguards-visage.png", shiny: "/item-shiny-vanguards-visage.png" },
+    { loot: "/item-sentinels-sidearm.png", shiny: "/item-shiny-sentinels-sidearm.png" },
+    { loot: "/item-bracer-of-the-guardian.png" },
+]
+
+let archLoot = [
+    { loot: "/item-dusky-catalyst.png" },
+    { loot: "/item-primal-arcana.png", shiny: "/item-shiny-primal-arcana.png" },
+    { loot: "/item-polarity-poison.png", shiny: "/item-shiny-polarity-poison.png" },
+    { loot: "/item-the-twilight-gemstone.png", shiny: "/item-shiny-the-twilight-gemstone.png" },
+]
+
+let kingLoot = [
+    { loot: "/item-warmonger.png", shiny: "/item-shiny-warmonger.png" },
+    { loot: "/item-peacekeeper.png", shiny: "/item-shiny-peacekeeper.png" },
+    { loot: "/item-noble-mandolin.png" },
+    { loot: "/item-the-forgotten-crown.png" },
+]
+
+let shattsLoot = [
+    { loot: "/item-royal-guards-cuirass.png" },
+    { loot: "/item-vest-of-abandoned-shadows.png" },
+    { loot: "/item-mantle-of-the-monarchy.png" },
+]
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center gap-y-20 p-20 text-center">
+        <div class="flex flex-row gap-x-5">
+            <div class="text-5xl font-bold">The Shatters</div>
+            <router-link :to="'/'" class="border px-5 hover:bg-red-600">
+                <NuxtImg :src="'/item-chrysalis-of-eternity.png'" class="w-12 h-12" />
+            </router-link>
+        </div>
+        <div class="flex flex-wrap gap-10 items-center">
+            <div 
+                v-for="(boss) in bosses"
+            >
+                <img :src="boss.image" class="w-40 h-40" />
+            </div>
+        </div>
+        <div class="flex flex-row gap-x-5">
+            <div class="text-5xl font-bold">Drops</div>
+            <NuxtImg :src="'/bag-white.png'" class="w-12 h-12" />
+        </div>
+        <div class="flex flex-col gap-10 items-center">
+            <div v-for="(boss, index) in bosses" :key="boss.link">
+                <div class="flex flex-row items-center gap-x-5">
+                    <NuxtImg :src="boss.image" class="w-21 h-21" />
+                    <div v-if="index === 0" class="flex flex-row gap-x-1">
+                        <div v-for="loot in bridgeLoot" class="flex flex-col gap-y-1">
+                            <NuxtImg :src="loot.loot" class="w-10 h-10" />
+                            <NuxtImg v-if="loot.shiny" :src="loot.shiny" class="w-10 h-10" />
+                        </div>
+                    </div>
+                    <div v-if="index === 1" class="flex flex-row gap-x-1">
+                        <div v-for="loot in archLoot" class="flex flex-col gap-y-1">
+                            <NuxtImg :src="loot.loot" class="w-10 h-10" />
+                            <NuxtImg v-if="loot.shiny" :src="loot.shiny" class="w-10 h-10" />
+                        </div>
+                    </div>
+                    <div v-if="index === 2" class="flex flex-row gap-x-1">
+                        <div v-for="loot in kingLoot" class="flex flex-col gap-y-1">
+                            <NuxtImg :src="loot.loot" class="w-10 h-10" />
+                            <NuxtImg v-if="loot.shiny" :src="loot.shiny" class="w-10 h-10" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
